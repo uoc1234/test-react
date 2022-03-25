49179c25f1d17fdddcb231a28e9cf812f0948922
vanuoc1996 <vanuoc9xhy@gmail.com>
2022-03-25 17:48:31 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>TEST REACT JS</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
